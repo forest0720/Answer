413b7f36f56a8d378541db59e475ba398e2fb06d
forest0720 <forest0720@users.noreply.github.com>
2020-07-15 09:46:00 +0900

    コメント機能　ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to:"sessions#index"
   devise_for :users
-  resources :top_page, only: [:index, :new, :create, :show]
+  resources :top_page, only: [:index, :new, :create, :show]do
+    resources :responses, only: :create
+  end
   resources :users, only: [:edit, :update]
 end
